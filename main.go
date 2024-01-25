74872eb35ef8a08d1f1a0d07a217c9e7bddd3281
satoshi <info.violasolo@gmail.com>
2024-01-25 22:40:45 +0900

    Add Retry

--- main.go
+++ main.go
@@ -1,18 +1,49 @@
 package main
 
 import (
-	//"fmt"
+	"database/sql"
+	"encoding/json"
+	"fmt"
 	"os"
 	"time"
 
 	"go.uber.org/zap"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+
+	"xicserver/handlers"
+
+	"github.com/gin-gonic/gin"
 )
 
 const maxRetries = 3                  // 最大再試行回数
 const retryInterval = 5 * time.Second // 再試行間の待機時間
-var logger *zap.Logger
+
+type Config struct {
+	DBHost     string `json:"db_host"`
+	DBUser     string `json:"db_user"`
+	DBPassword string `json:"db_password"`
+	DBName     string `json:"db_name"`
+	DBSSLMode  string `json:"db_sslmode"`
+}
+
+var (
+	logger *zap.Logger
+	sqlDB  *sql.DB // グローバル変数として定義
+)
+
+func LoadConfig(filename string) (Config, error) {
+	var config Config
+	configFile, err := os.Open(filename)
+	if err != nil {
+		return config, err
+	}
+	defer configFile.Close()
+
+	jsonParser := json.NewDecoder(configFile)
+	err = jsonParser.Decode(&config)
+	return config, err
+}
 
 func init() {
 	var err error
@@ -21,6 +52,53 @@ func init() {
 	if err != nil {
 		panic(err)
 	}
+}
+
+func initDB(config Config) (*gorm.DB, error) {
+	dsn := fmt.Sprintf("host=%s user=%s dbname=%s password=%s sslmode=%s",
+		config.DBHost, config.DBUser, config.DBName, config.DBPassword, config.DBSSLMode)
+
+	var err error
+	for i := 0; i <= maxRetries; i++ {
+		var gormDB *gorm.DB
+		gormDB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+		if err == nil {
+			sqlDB, err = gormDB.DB()
+			if err == nil {
+				return gormDB, nil
+			}
+		}
+
+		logger.Error("データベース接続のリトライ", zap.Int("retry", i), zap.Error(err))
+		time.Sleep(retryInterval)
+	}
+
+	return nil, fmt.Errorf("データベース接続に失敗しました: %v", err)
+
+	// host := os.Getenv("DB_HOST")
+	// user := os.Getenv("DB_USER")
+	// dbname := os.Getenv("DB_NAME")
+	// password := os.Getenv("DB_PASSWORD")
+	// sslmode := os.Getenv("DB_SSLMODE")
+
+	// dsn := "host=" + host + " user=" + user + " dbname=" + dbname + " password=" + password + " sslmode=" + sslmode
+
+	// var err error
+	// for i := 0; i <= maxRetries; i++ {
+	// 	var gormDB *gorm.DB
+	// 	gormDB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	// 	if err == nil {
+	// 		sqlDB, err = gormDB.DB()
+	// 		if err == nil {
+	// 			return gormDB, nil
+	// 		}
+	// 	}
+
+	// 	logger.Error("データベース接続のリトライ", zap.Int("retry", i), zap.Error(err))
+	// 	time.Sleep(retryInterval)
+	// }
+
+	// return nil, fmt.Errorf("データベース接続に失敗しました: %v", err)
 }
 
 // User モデルの定義
@@ -50,119 +128,25 @@ type GameRoom struct {
 func main() {
 	logger.Info("アプリケーションが起動しました。")
 
-	// 環境変数からデータベースの接続情報を取得
-	host := os.Getenv("DB_HOST")
-	user := os.Getenv("DB_USER")
-	dbname := os.Getenv("DB_NAME")
-	password := os.Getenv("DB_PASSWORD")
-	sslmode := os.Getenv("DB_SSLMODE")
+	defer sqlDB.Close()
+	defer logger.Sync()
 
-	dsn := "host=" + host + " user=" + user + " dbname=" + dbname + " password=" + password + " sslmode=" + sslmode
-	gormDB, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	config, err := LoadConfig("config.json")
 	if err != nil {
-		logger.Error("データベースへの接続に失敗しました", zap.Error(err))
+		logger.Fatal("設定ファイルの読み込みに失敗しました", zap.Error(err))
+	}
+
+	// データベース接続の初期化
+	_, err = initDB(config) // gormDB は現在使用しないため、変数に格納しない
+	if err != nil {
+		logger.Error("データベースの初期化に失敗しました", zap.Error(err))
 		return
 	}
 
-	sqlDB, err := gormDB.DB()
-	if err != nil {
-		logger.Error("SQLDBの取得に失敗しました", zap.Error(err))
-		return
-	}
-	// db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
-	// if err != nil {
-	// 	logger.Error("データベースへの接続に失敗しました", zap.Error(err))
-	// 	return
-	// }
+	// Ginエンジンの初期化
+	r := gin.Default()
+	// ルーティングの設定
+	r.POST("/gameroom", handlers.CreateGameRoom)
 
-	defer sqlDB.Close() // SQLDBを閉じる
-	//defer db.Close()   // データベース接続を閉じる
-	defer logger.Sync() // ロガーの終了処理
+	r.Run() // デフォルトでは ":8080" でリスニングします
 }
-
-// package main
-
-// import (
-// 	"fmt"
-// 	"os"
-// 	"time"
-
-// 	//"log"
-
-// 	"go.uber.org/zap"
-// 	"gorm.io/driver/postgres"
-// 	"gorm.io/gorm"
-// )
-
-// const maxRetries = 3                  // 最大再試行回数
-// const retryInterval = 5 * time.Second // 再試行間の待機時間
-// var logger *zap.Logger
-
-// func init() {
-// 	var err error
-// 	// Zapのロガーを設定
-// 	logger, err = zap.NewProduction() // または zap.NewDevelopment() で開発用の設定を行う
-// 	if err != nil {
-// 		panic(err)
-// 	}
-// 	logger = logger.WithOptions(zap.IncreaseLevel(zap.InfoLevel))
-// }
-
-// // User モデルの定義
-// type User struct {
-// 	gorm.Model                // gorm.Modelを埋め込むことでID、CreatedAt、UpdatedAt、DeletedAtフィールドが自動的に追加されます
-// 	UserID             string `gorm:"unique;not null"` // ユーザーID
-// 	SubscriptionStatus string `gorm:"not null"`        // 課金ステータス (無課金、スタンダード、プレミアムなど)
-// 	ValidRoomCount     int    `gorm:"not null"`        // 有効なルームの数
-// }
-
-// // テーブルの作成
-// func AutoMigrateDB(db *gorm.DB) {
-// 	err := db.AutoMigrate(&User{})
-// 	if err != nil {
-// 		panic("Error migrating User table: " + err.Error())
-// 	} else {
-// 		fmt.Println("User table created successfully")
-// 	}
-// 	// 他のモデルに対しても同様にAutoMigrateを呼び出すことができますx
-// }
-
-// func main() {
-// 	logger.Info("アプリケーションが起動しました。")
-
-// 	// 環境変数からデータベースの接続情報を取得
-// 	host := os.Getenv("DB_HOST")
-// 	user := os.Getenv("DB_USER")
-// 	dbname := os.Getenv("DB_NAME")
-// 	password := os.Getenv("DB_PASSWORD")
-// 	sslmode := os.Getenv("DB_SSLMODE")
-
-// 	dsn := "host=" + host + " user=" + user + " dbname=" + dbname + " password=" + password + " sslmode=" + sslmode
-
-// 	var db *gorm.DB
-// 	var err error
-// 	for i := 0; i < maxRetries; i++ {
-// 		db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
-// 		if err == nil {
-// 			break
-// 		}
-// 		logger.Error("データベースへの接続に失敗しました", zap.Int("retry", i), zap.Error(err))
-// 		time.Sleep(retryInterval)
-// 	}
-// 	if err != nil {
-// 		logger.Fatal("データベースへの接続に最終的に失敗しました", zap.Error(err))
-// 		return
-// 	}
-
-// 	// テーブル 'users' が存在するかを確認する
-// 	var exists bool
-// 	db.Raw("SELECT exists (SELECT 1 FROM information_schema.tables WHERE table_name = 'users')").Scan(&exists)
-// 	if exists {
-// 		logger.Info("Table 'users' exists.")
-// 	} else {
-// 		logger.Info("Table 'users' does not exist.")
-// 	}
-
-// 	AutoMigrateDB(db)
-
-// 	defer logger.Sync()
