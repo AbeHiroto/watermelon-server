1f2733fe411b1909ee555b786ce313ed23513678
satoshi <info.violasolo@gmail.com>
2024-04-18 04:53:36 +0900

    First push

--- auth/auth.go
+++ auth/auth.go
@@ -6,8 +6,6 @@ import (
 	jwt "github.com/dgrijalva/jwt-go"
 )
 
-//var JwtKey = []byte("your_secret_key") // 本番環境では安全に管理
-
 func IsValidToken(tokenString string) (bool, error) {
 	claims := &models.MyClaims{}
 
